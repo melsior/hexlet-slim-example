bfb81800aa4daacb4e1a1b62fc9dbee46fac92eb
Alexander Vasilyev <melsior@gmail.com>
2018-12-07 16:14:19 +0000

    from post form

--- public/index.php
+++ public/index.php
@@ -9,9 +9,25 @@ $configuration = [
 ];
 
 $app = new \Slim\App($configuration);
+$repo = new Repository();
 
+$app->post('/users', function ($request, $response) use ($repo) {
+    $validator = new Validator();
+    $user = $request->getParsedBodyParam('user');
+    $errors = $validator->validate($user);
+    if (count($errors) === 0) {
+        $repo->save($user);
+        return $response->withRedirect('/');
+    }
+    $params = [
+        'user' => $user,
+        'errors' => $errors
+    ];
+    return $this->renderer-render($response, "users/new.phtml", $params);
+});
 $app->get('/', function ($request, $response) {
     $response->write('Welcome to Slim!');
     return $response;
 });
+
 $app->run();
